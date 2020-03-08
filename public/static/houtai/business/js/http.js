b9d5a9685e0a09da81bffb22ceee7dd2191022f6
苟富军 <750108747@qq.com>
2020-03-08 22:37:02 +0800

    上传图片 ok

---
+++ public/static/houtai/business/js/http.js
@@ -0,0 +1,19 @@
+/** post jQuery ajax */
+function http_post(url, data) {
+    return new Promise((reslove, reject) => {
+        $.ajax({
+            url: url,
+            type:'post',
+            data: data,
+            dataType:'json',
+            success:function (res) {
+                if (res.code == 1)  reslove(res);
+                reject(res);
+            },
+            error: function (err) {
+                console.log(err)
+                layer.msg('出错了，状态 status:' + err.status);
+            }
+        });
+    })
+}
