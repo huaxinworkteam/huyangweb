a06dceda02777f753fde0c4a836216c3f51bc32e
马鹏飞 <937096063@qq.com>
2020-03-09 21:28:17 +0800

    asdasd

--- application/common/model/Ad_position.php
+++ application/common/model/Ad_position.php
@@ -12,4 +12,22 @@ use think\model;
 class Ad_position extends  model
 {
     protected $pk='positionId';
+
+    public  function add($data){
+        if(!$data) return false;
+        $validate =new \app\common\validate\Ad_position();
+        if(!$validate->check($data)){
+            return $validate->getError();
+        }
+        if($data['positionId']) $res=$this->where('positionId',$data['positionId'])->allowField(true)->update($data);
+     else   $res=$this->allowField(true)->save($data);
+        if($res){
+            return 1;
+        }else{
+            return '发送失败';
+        }
+
+
+    }
+
 }
---
+++ application/common/validate/Ad_position.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 马鹏飞
+ * Date: 2020/3/9
+ * Time: 20:32
+ */
+
+namespace app\common\validate;
+
+
+use think\Validate;
+
+class Ad_position extends Validate
+{
+  protected  $rule=[
+      'positionType|位置类型'=>'require',
+      'positionName|位置名称'=>'require',
+      'positionCode|位置代码'=>'require',
+      'positionWidth|建议宽度'=>'require',
+      'positionHeight|建议高度'=>'require',
+      ];
+
+}
