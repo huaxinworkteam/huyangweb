00b99601fc46206b08f88ba903adea574b3c114b
马鹏飞 <937096063@qq.com>
2020-03-12 14:16:09 +0800

    asdasd

--- application/common/model/CourseType.php
+++ application/common/model/CourseType.php
@@ -67,27 +67,6 @@ class CourseType extends Model
             return 1;
     }
 
-    //TODO 寻找该层极其所有孩子们,调用完必须清空树,field必须有nextId,lastId
- /*   public static function findAllSons($typeLevel = 1, $field = '*'){
-       // self::$startlevel=$typeLevel;
-        $arr=self::where(['isDel'=>0,'typeLevel'=>$typeLevel])->whereIn('lastId',self::$record)->field($field)->select()->toArray();
-        $size=sizeof($arr);
-        for($i=0;$i<$size;$i++){
-           array_push(self::$tree,json_encode(['typeLevel'=>$typeLevel,'data'=>$arr[$i]]));
-           self::$record.=$arr[$i]['id'].',';
-            if($arr[$i]['nextId']==null&&($i+1)==$size)
-            {
-                $typeLevel-=1;
-            }
-            if($arr[$i]['nextId']!=null)
-            {
-                $typeLevel+=1;
-                self::findAllSons($typeLevel,$field);
-            }
-        }
-
-        return json_encode(self::$tree);
-    }*/
 
     //获取全表信息
     public static function getAll($typeLevel=1)
@@ -119,7 +98,7 @@ class CourseType extends Model
     //获取指定信息
     public static function getOne($id)
     {
-       self::where(['id'=>$id])->field('id,typeName')
+     //  self::where(['id'=>$id])->field('id,typeName')
 
     }
 
