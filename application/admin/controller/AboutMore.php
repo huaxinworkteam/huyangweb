af6e9d1cc42a9ffa50105acc1e485a3adabf0b27
马鹏飞 <937096063@qq.com>
2020-03-13 21:37:20 +0800

    asdsadas

---
+++ application/admin/controller/AboutMore.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 马鹏飞
+ * Date: 2020/3/13
+ * Time: 20:54
+ */
+
+namespace app\admin\controller;
+use app\common\model\AboutMore as AboutMoreModel;
+
+class AboutMore extends Base
+{
+        public function index(){
+            if(request()->isAjax()){
+                $res=AboutMoreModel::getAll();
+                if($res) return myJson('T',$res);
+                else return myJson('F','未获取任何数据');
+            }
+            return view();
+        }
+
+        public function  edit(){
+            $id=input('id');
+            if(request()->isAjax()){
+                $res=AboutMoreModel::getOne($id);
+                if($res) return myJson('T',$res);
+                else return myJson('F','获取数据失败');
+            }
+            $this->assign('id',$id);
+            return view();
+        }
+
+        public function saveInfo(){
+            $data=[
+              'id'=>input('id')? input('id'):'',
+            'name'=>input('name'),
+             'content'=>input('content'),
+                'isShow'=>input('isShow'),
+                'sort'=>input('sort'),
+                'updateTime'=>time()
+            ];
+            $res=AboutMoreModel::saveInfo($data);
+            if($res===1) return myJson('T','保存成功');
+            else return myJson('F',$res);
+        }
+
+        public  function del(){
+            $id=input('id');
+            $res=AboutMoreModel::del($id);
+            if($res) return myJson('T','删除成功');
+            else return myJson('F','删除失败');
+        }
+}
--- application/common/model/AboutMore.php
+++ application/common/model/AboutMore.php
@@ -13,5 +13,43 @@ use think\Model;
 
 class AboutMore extends Model
 {
+    public static function saveInfo($data){
+            $validate=new \app\common\validate\AboutMore();
+            if(!$validate->check($data)){
+                return $validate->getError();
+            }
 
+            try {
+                if ($data['id']) {
+                    $res = self::where('id', $data['id'])->update($data);
+                } else {
+                    unset($data['id']);
+                    $res = model('AboutMore')->insert($data);
+                }
+            }catch (\Exception $e){
+                return $e->getMessage();
+            }
+            return 1;
+
+    }
+
+    public static function getAll(){
+       $res=self::where(['isDel'=>0])->field('id,name,isShow,sort')->order('sort desc')->select();
+       if($res)
+       return $res;
+       else return 0;
+    }
+
+    public static function getOne($id){
+        $res=self::where('id',$id)->field('id,name,content,isShow,sort')->find();
+        if($res) return $res;
+        return 0;
+    }
+
+    public static function del($id){
+        $id=','.$id.',';
+        $res=model('AboutMore')->whereIn('id',$id)->update(['isDel'=>1]);
+        if($res) return 1;
+        else return 0;
+    }
 }
