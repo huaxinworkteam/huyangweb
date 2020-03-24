10986959a4e3421236c6aef78c7e79febfef20d0
sulifer <937096063@qq.com>
2020-03-24 16:41:47 +0800

    'asdasdasd'

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -68,8 +68,9 @@ class Index extends Controller
         if(request()->isAjax()) {
             $p=input('page');
             if(!$p) return myJson('F','参数异常');
-            $all_activity = Db::connect('db_config1')->name('fx_activity')->field('id,title,thumb,intro,starttime,endtime')->where(['show'=>1,'merchantid'=>19])->order('displayorder desc')->limit(($p->current_page-1)*$p->per_page+1,$p->current_page*$p->per_page);
-            if($all_activity) return myJson('T',$all_activity);
+            $all_activity = Db::connect('db_config1')->name('fx_activity')->field('id,title,thumb,intro,starttime,endtime')->where(['show'=>1,'merchantid'=>19])->order('displayorder desc')->limit(($p['current_page']-1)*$p['per_page']+1,$p['current_page']*$p['per_page'])->select();
+            $total=Db::connect('db_config1')->where(['show'=>1,'merchantid'=>19])->name('fx_activity')->count();
+            if($all_activity) return myJson('T',['info'=>$all_activity,'total'=>$total]);
             else return myJson('F','暂无数据');
         }
         return view('chhcollege/activity/index');
