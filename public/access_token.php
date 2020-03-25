ff66a2efe8f7e2a68258d1cb706dfea4d81d409d
goufujun <fujun_gou@foxmail.com>
2020-03-25 15:55:22 +0800

    fix(index): 分页 分类 逻辑修复

--- public/access_token.php
+++ public/access_token.php
@@ -1,3 +1,5 @@
 <?php return array (
-  'get_time' => 1585111336,
+  'access_token' => 'xe_5e7b0e596ec68_Op05suSv7usIsKeqoOdncooaZLlhDD2T2UtVyQmvxs6T6WjWGcyX9HJ1uXShg71SSD1MQFM6OwVZlgU4Z1QEPdmNX5Tyt7emX39aziHtB2sy3M2KLypvTDDSdPNM9QlrUEGmagxdJReKuO68dkngoEny',
+  'expires_in' => 7200,
+  'get_time' => 1585122905,
 );
