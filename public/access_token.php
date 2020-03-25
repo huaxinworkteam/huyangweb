7c12b58019f7b61a678cfe12f2378cdab705d4ca
goufujun <fujun_gou@foxmail.com>
2020-03-25 12:43:56 +0800

    style(): 师资力量

--- public/access_token.php
+++ public/access_token.php
@@ -1,5 +1,3 @@
 <?php return array (
-  'access_token' => 'xe_5e75bf6fe9d78_mXP3Gr4AZAedKfswrFLkEcblRSUPwKnTKxCYQQixVwihGzhVLxstbDxfbSIUkx5KkriOpkGW4qkxlEgp8AVPbTNvuv8LkHltUFyg9TagOsdc0ltlwrjnpEZM9hpMD7tSBjo',
-  'expires_in' => 7200,
-  'get_time' => 1584775024,
+  'get_time' => 1585111336,
 );
