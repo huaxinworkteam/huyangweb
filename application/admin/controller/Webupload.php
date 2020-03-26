416f4497fc05a1c9fc298001661afa229f4f7b37
sulifer <937096063@qq.com>
2020-03-26 16:54:12 +0800

    'asdasd'

--- application/admin/controller/Webupload.php
+++ application/admin/controller/Webupload.php
@@ -58,4 +58,20 @@ class Webupload extends Controller
             }
         }
     }
+    //获取院系图片路径
+    public function seriesUpload(Request $request)
+    {
+        //获取图片对象
+        $filetemp = $request->file('file');
+        if ($filetemp) {
+            //存放服务器上地址
+            $serverFile = $filetemp->move(ROOT_PATH . '/public/upload/seriesIcon');
+            if($serverFile) {
+                //访问地址
+                return json_encode($serverFile->getSaveName());
+            }else{
+                echo $filetemp->getError();
+            }
+        }
+    }
 }
