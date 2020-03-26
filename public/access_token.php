e84fddbad845f49d5e17b978bb1533c752192b51
goufujun <fujun_gou@foxmail.com>
2020-03-26 12:03:58 +0800

    style(*): 清缓存了

--- public/access_token.php
+++
@@ -1,5 +0,0 @@
-<?php return array (
-  'access_token' => 'xe_5e7c0a0949255_KALjvURUwe1IzV2almqFbfY23WBI5Gx3Z2ad9ImPniW0sTR01ZPlg4z0PZ1zGP3NPxQxCfPmhuwPC6CFsq6Z9KdPFNUyAvsACEKOjMTqKY0LfasBV6kQO5v39QzNJEQ7pcKXir1wZSq87uzkIMvAdv29iVEyOPE',
-  'expires_in' => 7200,
-  'get_time' => 1585187336,
-);
