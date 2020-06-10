9845369b5ea07ea5f1eca71f0ce80f0801c73378
goufujun <fujun_gou@foxmail.com>
2020-06-10 10:26:30 +0800

    '百度统计js文件'

--- public/static/chhcollege/js/baidustatistics.js
+++
@@ -1,7 +0,0 @@
-var _hmt = _hmt || [];
-(function () {
-  var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?19436ea9121cf944ec3d8bc118b9904f";
-  var s = document.getElementsByTagName("script")[0];
-  s.parentNode.insertBefore(hm, s);
-})();
